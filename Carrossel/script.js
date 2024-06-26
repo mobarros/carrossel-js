eeaf2f2f79532530f86d415b6a409b9cd2b7a2fd
Monique Barros <99218248+mobarros@users.noreply.github.com>
2024-06-26 00:04:59 -0300

    Add files via upload

---
+++ Carrossel/script.js
@@ -0,0 +1,17 @@
+const box = document.querySelector(".container");
+const imagens = document.querySelectorAll(".container img");
+
+let contador = 0;
+
+function slider() {
+    contador++;
+
+    if (contador > imagens.length -1) {
+        contador = 0;
+    }
+
+    box.style.transform = `translateX(${-contador * 500}px)`;
+}
+
+
+setInterval(slider , 3000);
